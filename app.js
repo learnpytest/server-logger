8369678da2def3fd6777755426b3724a848ce370
RachelChen <iceage91@gmail.com>
2021-06-22 19:57:32 +0800

    feat: add response time to log

--- app.js
+++ app.js
@@ -3,12 +3,35 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const onFinished = require('on-finished')
+
 app.set('trust proxy', true)
 
 const logger = (req, res, next) => {
-  const timeForRequest = new Date().toLocaleString()
-  console.log(`${timeForRequest} network info HTTP | ${req.method} from ${req.path} | IP is ${req.ip} on ${req.get('host')}`)
+  const time = new Date()
+  const timeStampForRequest = time.getTime()
+  const localTimeForRequest = time.toLocaleString()
   next()
+
+  onFinished(res, function (err, res) {
+    if (err) return console.log(err)
+    const timeStampForResponse = new Date().getTime()
+    const responseTimeGap = timeStampForResponse - timeStampForRequest
+    console.log(`Use onFinished npm too: ${localTimeForRequest} | ${req.method} from ${req.path} | total time: ${responseTimeGap}ms`)
+  })
+  res.on('finish', () => {
+    const timeStampForResponse = new Date().getTime()
+    const responseTimeGap = timeStampForResponse - timeStampForRequest
+    console.log(`Use res.on finish: ${localTimeForRequest} | ${req.method} from ${req.path} | total time: ${responseTimeGap}ms`)
+  })
+  res.on('close', () => {
+    const timeStampForResponse = new Date().getTime()
+    const responseTimeGap = timeStampForResponse - timeStampForRequest
+    console.log(`Use res.on close: ${localTimeForRequest} | ${req.method} from ${req.path} | total time: ${responseTimeGap}ms`)
+  })
+  const timeStampForResponse = new Date().getTime()
+  const responseTimeGap = timeStampForResponse - timeStampForRequest
+  console.log(`Console log directly: ${localTimeForRequest} | ${req.method} from ${req.path} | total time: ${responseTimeGap}ms`)
 }
 
 app.use(logger)
@@ -27,6 +50,10 @@ app.get('/:id', (req, res) => {
 
 app.post('/', (req, res) => {
   res.send('新增一筆  Todo')
+})
+//
+app.use((req, res) => {
+  res.end()
 })
 
 app.listen(port, () => {
